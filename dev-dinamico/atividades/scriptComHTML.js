abb2cf95272a316b6400d329f15987fda0f5b912
zirtaebea <zirteb.dev@gmail.com>
2024-02-13 18:12:59 -0300

    javascript-aula-2

---
+++ dev-dinamico/atividades/scriptComHTML.js
@@ -0,0 +1,3 @@
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Qual a sua idade?");
+alert(`Seu nome é ${nome} e sua idade é ${idade}`);
---
+++ dev-dinamico/atividades/scriptComNode.js
@@ -0,0 +1,3 @@
+let rlSync = require("readline-sync");
+let nome = rlSync.question("Qual o seu nome? \n")
+console.log(`Olá ${nome}`)
